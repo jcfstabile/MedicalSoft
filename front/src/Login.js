815ddf5e884eb9693f9c1997eb800a9ee4207ec2
Thomas Lautaro López <laautarolopez@gmail.com>
2022-04-23 21:51:54 -0300

    agregado container, box, y campos input

--- front/src/Login.js
+++ front/src/Login.js
@@ -4,10 +4,18 @@ const Login = () => {
     return (
         <div className='container'>
             <div className="box">
-                <div className="logo"></div>
+                <div className="logo">
+                    <img src='/images/logo.png' alt='' width="100%" />
+                </div>
                 <p>INICIAR SESIÓN</p>
-                <div className="campo"></div>
-                <div className="campo"></div>
+                <div className="campo">
+                    <img src='/images/usuario.png' alt='' width="100%" />
+                    <input type="text" name='user' placeholder='Usuario' />
+                </div>
+                <div className="campo">
+                    <img src='/images/bloquear.png' alt='' width="100%" />
+                    <input type="password" name='password' placeholder='Contraseña' />
+                </div>
                 <div className="boton">INGRESAR</div>
             </div>
         </div>
