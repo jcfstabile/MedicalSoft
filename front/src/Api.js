0757bd57c08bc3ee75f4789beec19b0cdacea864
Thomas Lautaro López <laautarolopez@gmail.com>
2022-04-25 16:53:14 -0300

    cambios api

--- front/src/Api.js
+++ front/src/Api.js
@@ -2,15 +2,15 @@
 
 import axios from 'axios'
 
-const host = 'http://localhost:3000';
+const host = 'http://localhost:7777';
 
 
 function agregarPaciente( payload){
-    return axios.post(host.concat('/agregarpaciente'), payload)
+    return axios.post(host.concat('/api/paciente'), payload)
 }
 
 function login (datosUsuario){
-    return axios.post(host.concat('/login'),datosUsuario)
+    return axios.post(host.concat('/api/login'),datosUsuario)
 }
 
 export default{
