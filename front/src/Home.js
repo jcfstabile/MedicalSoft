ad031dc10ddebb03525bc3f366acd26dfd7fb0ac
DoomSlayer01 <leandrofranco97@outlook.es>
2022-05-21 22:34:02 -0300

    agrego css home

--- front/src/Home.js
+++ front/src/Home.js
@@ -5,18 +5,18 @@ const Home = (props) => {
     return (
         <div className='contenedor'>
             <div className='nav'>
-                <p>MedicalSoft</p>
-                <img src='/images/electro.png' alt='' width="100%" />
+                <img className="navlogo" src='/images/logoNombre.png' alt='' width="100%" />
                 <div className='nav-boton'>
                     <span>Pacientes</span>
                     <div class="submenu">
+                        <img src='/images/hospitalizacion.png' alt='' width="100%" />
                         <Link to='/agregarPaciente'>Agregar paciente</Link>
                         <Link to='/buscarPaciente'>Buscar paciente</Link>
                     </div>
                 </div>
             </div>
             <div className='imagenLogo'>
-                <img src='/images/logoLogin.png' alt='' width="100%" />
+                <img src='/images/logoNombre.png' alt='' width="100%" />
             </div>
         </div>
     );
